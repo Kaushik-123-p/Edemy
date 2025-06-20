f628abb0256869ec010e50016d4845e992fa6302
kaushik-p-123 <prajapatikaushik6292@gmail.com>
2025-06-20 19:30:36 +0530

    Implement routing for student and educator pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/student/Home.jsx";
+import CoursesList from "./pages/student/CoursesList.jsx";
+import CourseDetails from "./pages/student/CourseDetails.jsx";
+import MyEnrollments from "./pages/student/MyEnrollments.jsx";
+import Player from "./pages/student/Player.jsx";
+import Loading from "./components/students/Loading.jsx";
+
+import Educator from "./pages/educator/Educator.jsx";
+import Dashboard from "./pages/educator/Dashboard.jsx";
+import AddCourse from "./pages/educator/AddCourse.jsx";
+import MyCourses from "./pages/educator/MyCourses.jsx";
+import StudentsEnrolled from "./pages/educator/StudentsEnrolled.jsx";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl ">CourseFlow</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CoursesList />} />
+        <Route path="/course-list/:input" element={<CoursesList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollments />} />
+        <Route path="/player/:courseId" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+
+        <Route path="/educator" element={<Educator />}>
+          <Route path="educator" element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="student-enrolled" element={<StudentsEnrolled />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
