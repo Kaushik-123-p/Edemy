122d38e76cc6447ee641a7ca3ed54287e735ef8c
kaushik-p-123 <prajapatikaushik6292@gmail.com>
2025-06-24 11:47:24 +0530

    Added CourseCard for course details. CoursesSection now fetches and displays courses from AppContext. Integrated currency formatting. Updated Tailwind config for responsive course grid.

--- client/src/components/students/CoursesSection.jsx
+++ client/src/components/students/CoursesSection.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import CourseCard from "./CourseCard";
+import { AppContext } from "../../context/AppContext";
 
 const CoursesSection = () => {
+  const { allCourses } = useContext(AppContext);
+
   return (
     <div className="py-16 md:px-40 px-8">
       <h2 className="text-3xl font-medium text-gray-800">
@@ -9,14 +13,22 @@ const CoursesSection = () => {
       </h2>
       <p className="text-sm md:text-base text-gray-500 my-5">
         Discover our top-rated courses across various categories, from coding
-        and design to business and wellness, our courses are crafted to deliver
-        results.
+        and design to <br /> business and wellness, our courses are crafted to
+        deliver results.
       </p>
+      <div
+        className="grid w-full grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-4 md:px-0 md:my-16 my-10 gap-4 md:gap-6"
+        aria-label="Featured Courses"
+      >
+        {allCourses.slice(0, 4).map((course, index) => (
+          <CourseCard key={index} course={course} />
+        ))}
+      </div>
 
       <Link
         to="/course-list"
         onClick={() => scrollTo(0, 0)}
-        className="text-gray-500 border border-gray-500/30 px-10 py-3 rounded "
+        className="text-gray-500 border border-gray-500/30 px-10 py-3 rounded hover:bg-blue-500 hover:text-white transition-colors duration-6 00"
       >
         Show all courses
       </Link>
