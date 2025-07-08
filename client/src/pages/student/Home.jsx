94cdf909b0c41a8a75e8be904a7fdd081f29901d
kaushik-p-123 <prajapatikaushik6292@gmail.com>
2025-07-08 10:10:55 +0530

    Add CallToAction component to Home page for improved user engagement

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../../components/students/Hero";
 import Companies from "../../components/students/Companies";
 import CoursesSection from "../../components/students/CoursesSection";
 import TestimonialsSection from "../../components/students/TestimonialsSection";
+import CallToAction from "../../components/students/CallToAction";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Companies />
       <CoursesSection />
       <TestimonialsSection />
+      <CallToAction />
     </div>
   );
 };
